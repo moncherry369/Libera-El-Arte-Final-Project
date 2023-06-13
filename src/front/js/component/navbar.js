635f567a0025f16958aaad17e37e5125f34207d5
moncherry369 <moncherry8675309@gmail.com>
2023-06-13 01:54:50 +0000

    needs css and backend. links and layout added.

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import reactDom from "react-dom";
+import { Dropdown } from "bootstrap";
+import "../../img/lea-navbar-logo.jpg";
+import "./navbar.css"
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <div className="navbar-logo">
+            <img className="" href="#top" src="https://i.ibb.co/v3992Zz/lea-logo.jpg" id="navbarImgLogo"/>
+          </div>
+          <h2 className="brand-text">Libera El Arte</h2>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item" href="#">Browse Collections</a>
+            <a className="navbar-item" href="#">Our Mission</a>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <div className="user-navbar-info">
+                <img className="rounded-circle" src="https://placekitten.com/122" id="nav-profpic"/>
+              </div>
+              <a className="navbar-dropdown-link">Your Atelier</a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item dropdown-item" href="#">Your creations</a>
+                <a className="navbar-item dropdown-item" href="#">Beloved Works</a>
+                <a className="navbar-item dropdown-item" href="#">Settings</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
