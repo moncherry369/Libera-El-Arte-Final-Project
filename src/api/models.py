c81152dbb5f50f89357a2ea4525ff6acc2b1b8fc
moncherry369 <moncherry8675309@gmail.com>
2023-06-15 00:50:49 +0000

    Settings model added along with login/signup and s3 routes

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,49 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+beloved_pieces = db.Table(
+    "beloved_pieces",
+   
+    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("piece", db.ForeignKey("piece.id")),
+)
+
+
+beloved_collections = db.Table(
+    "beloved_collections",
+   
+    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("collection", db.ForeignKey("collection.id")),
+)
+
+user_collections = db.Table(
+    "user_collections",
+
+    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("collection", db.ForeignKey("collection.id"))
+)
+
+collection_pieces = db.Table(
+"collection_pieces",
+
+db.Column("collection_id", db.ForeignKey("collection.id")),
+db.Column("piece_id", db.ForeignKey("piece.id"))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    # Profile Picture
+
+    name = db.Column(db.String(250), unique= False, nullable= False)
+    phone_number = db.Column(db.Integer, unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    beloved_pieces = db.relationship('Piece',secondary="beloved_pieces")
+    beloved_collections = db.relationship('Collection',secondary="beloved_collections")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +53,45 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "phone_number": self.phone_number,
             # do not serialize the password, its a security breach
         }
+
+
+class Piece(db.Model):
+    __tablename__ = "piece"
+    id = db.Column(db.Integer, primary_key=True)
+    # user_id = db.Column(db.Integer,ForeignKey('user.id'))
+    title = db.Column(db.String(250))
+    description = db.Column(db.String(250))
+    upload_date = db.Column(db.DateTime)
+    beloved_by = db.Column(db.Integer)
+    # piece = db.relationship('Piece',secondary="beloved_pieces")
+
+class Collection(db.Model):
+    __tablename__ = 'collection'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250))
+    description = db.Column(db.String(250))
+    beloved_by = db.Column(db.Integer)
+    collection = db.relationship('Piece',secondary="collection_pieces")
+
+class Settings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(256), unique=True)
+    value = db.Column(db.JSON)
+
+
+
+class MediaType(db.Enum):
+    IMAGE = 'image'
+    VIDEO = 'video'
+    AUDIO = 'audio'
+
+
+class Media(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(256), unique=True)
+    filetype = db.Column(db.Enum("image", "video", "audio", name="MediaType"), default="image")
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,126 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Settings
 from api.utils import generate_sitemap, APIException
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+#       !!--USER--!!
+@api.route('/login', methods=['POST'])
+def handle_login():
+    print("body:", request.json)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({
+            "msg": "No account was found. Please check the email used or create an account."
+        }), 401
+    
+    if password != user.password:
+        return jsonify({"msg": "Incorrect password. Please try again."}), 401
+
+    access_token = create_access_token(identity=email)
+    
+    payload = {
+        "token": access_token,
+        'msg': 'Login Successful.',
     }
+    return jsonify(payload), 200
 
-    return jsonify(response_body), 200
+# !!--SIGN-UP--!!
+@api.route('/signup', methods=['POST'])
+def handle_signup():
+    print("body:", request.json)
+
+    body = request.json # get the request body content
+    email = body.get('email', None)
+    name = body.get('name', None)
+    password = body.get('password', None)
+    
+    if body is None:
+        return "The request body is null", 400
+    if not email:
+        return 'You need to enter an email',400
+    if not name:
+        return 'You need to enter an name',400
+    if not password:
+        return 'You need to enter a password', 400
+
+    check_user = User.query.filter_by(email=email).first()
+
+    if check_user is not None:
+        return jsonify({
+            'msg': 'The email address already exists. Please login to your account to continue.'
+        }), 409
+
+    user = User(email=email, password=password, is_active=True)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        access_token = create_access_token(identity=user.email)
+        refresh_token = create_refresh_token(identity=user.email)
+        return jsonify({
+            'msg': 'User created successfully.',
+            'access_token': access_token,
+            'refresh_token': refresh_token
+         }), 201
+
+    except Exception as e:
+        return jsonify({'msg': 'An error occurred while creating the user.'}), 500
+
+# AWS S3 CONNECTION
+def upload_file_to_s3(file_bytes, object_name):
+    aws_info = Setting.query.filter_by(key="aws/secrets").first()
+    s3_client = boto3.resource(
+        's3',
+        aws_access_key_id=aws_info.value["s3AccessKey"],
+        aws_secret_access_key=aws_info.value["s3SecretKey"]
+    )
+    bucket = s3_client.Bucket(aws_info.value["s3BucketName"])
+    try:
+        bucket.upload_fileobj(file_bytes, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+  
+@api.route('/assets', methods=['POST'])
+def upload_file():
+    # Check if there is a file.
+    if 'file' not in request.files:
+        return jsonify(msg='No file found'), 400
+    request_file = request.files['file']
+
+    # Check if the filename is valid.
+    filename = secure_filename(request_file.filename)
+    if filename == '':
+        return jsonify(msg='Invalid filename'), 400
+
+    # Attempt to upload the file to S3.
+    if upload_file_to_s3(request_file, filename):
+        dbfile = Media(
+            filename=filename,
+            filetype="IMAGE"
+        )
+        db.session.add(dbfile)
+        db.session.commit()
+    return '', 204
+  
+@api.route('/assets/<string:filename>', methods=['GET'])
+def get_file_URL(filename: str):
+    aws_info = Setting.query.filter_by(key="aws/secrets").first()
+    file_info = Media.query.filter_by(filename=filename).first()
+    return jsonify(
+        url=f"""https://{aws_info.value["distDomainName"]}/{filename}""",
+        media_type=file_info.filetype.value,
+    )
